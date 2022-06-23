d1da7b23d41aa9ecc1d5872d98d0c37562c028a2
Alexandre Lima <alexandreatlima@gmail.com>
2022-06-23 11:42:12 -0300

    forms

--- md02/lift-state/src/App.js
+++ md02/lift-state/src/App.js
@@ -4,14 +4,21 @@ import { useState } from "react";
 
 function App() {
   const [value, setValue] = useState(5.2);
+  const [currency, setCurrency] = useState("Dolar");
 
   return (
     <div className="App">
-      <Screen currency="dolar" value={value} />
+      <Screen currency={currency} value={value} />
       <div className="d-flex justify-content-evenly">
-        <Button setValue={setValue}>Dolar</Button>
-        <Button setValue={setValue}>Euro</Button>
-        <Button setValue={setValue}>Libra</Button>
+        <Button setValue={setValue} setCurrency={setCurrency}>
+          Dolar
+        </Button>
+        <Button setValue={setValue} setCurrency={setCurrency}>
+          Euro
+        </Button>
+        <Button setValue={setValue} setCurrency={setCurrency}>
+          Libra
+        </Button>
       </div>
     </div>
   );
--- md02/lift-state/src/components/Button/index.js
+++ md02/lift-state/src/components/Button/index.js
@@ -2,16 +2,19 @@ export function Button(props) {
   function handleButton() {
     if (props.children === "Dolar") {
       props.setValue(5.2);
+      props.setCurrency(props.children);
       return;
     }
 
     if (props.children === "Libra") {
       props.setValue(6.37);
+      props.setCurrency(props.children);
       return;
     }
 
     if (props.children === "Euro") {
       props.setValue(5.46);
+      props.setCurrency(props.children);
       return;
     }
   }
